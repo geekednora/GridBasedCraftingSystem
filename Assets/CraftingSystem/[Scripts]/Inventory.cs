43fbb5837de7c87a3160892931c0155b57979a9f
Andrii Gastello <concof.zp@gmail.com>
2023-11-17 22:50:39 -0500

    Rearranged namespaces, added CraftingSystem.cs

--- Assets/CraftingSystem/[Scripts]/Inventory.cs
+++ Assets/CraftingSystem/[Scripts]/Inventory.cs
@@ -1,107 +1,105 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace CraftingSystem
+public class Inventory : MonoBehaviour
 {
-    public class Inventory : MonoBehaviour
+    List<ItemSlot> itemSlots = new List<ItemSlot>();
+
+    [SerializeField]
+    GameObject inventoryPanel;
+
+    void Start()
     {
-        List<ItemSlot> itemSlots = new List<ItemSlot>();
+        //Read all itemSlots as children of inventory panel
+        itemSlots = new List<ItemSlot>(
+            inventoryPanel.transform.GetComponentsInChildren<ItemSlot>()
+        );
+    }
 
-        [SerializeField]
-        GameObject inventoryPanel;
-
-        void Start()
+    public bool CraftItem(List<Item> recipe, Item outputItem, int outputCount)
+    {
+        // Check if the inventory contains the required items for the recipe
+        if (CheckRecipe(recipe))
         {
-            //Read all itemSlots as children of inventory panel
-            itemSlots = new List<ItemSlot>(
-                inventoryPanel.transform.GetComponentsInChildren<ItemSlot>()
-            );
+            // Remove items from the inventory based on the recipe
+            RemoveItemsFromInventory(recipe);
+
+            // Add the crafted item to the inventory
+            AddItemToInventory(outputItem, outputCount);
+
+            return true;
         }
-    
-        public bool CraftItem(List<Item> recipe, Item outputItem, int outputCount)
+        else
         {
-            // Check if the inventory contains the required items for the recipe
-            if (CheckRecipe(recipe))
-            {
-                // Remove items from the inventory based on the recipe
-                RemoveItemsFromInventory(recipe);
+            Debug.Log("Crafting failed. Missing required items.");
+            return false;
+        }
+    }
 
-                // Add the crafted item to the inventory
-                AddItemToInventory(outputItem, outputCount);
-
-                return true;
-            }
-            else
+    private bool CheckRecipe(List<Item> recipe)
+    {
+        // Check if the inventory contains the required items for the recipe
+        foreach (Item recipeItem in recipe)
+        {
+            if (!ContainsItem(recipeItem))
             {
-                Debug.Log("Crafting failed. Missing required items.");
                 return false;
             }
         }
-
-        private bool CheckRecipe(List<Item> recipe)
-        {
-            // Check if the inventory contains the required items for the recipe
-            foreach (Item recipeItem in recipe)
-            {
-                if (!ContainsItem(recipeItem))
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        private void RemoveItemsFromInventory(List<Item> items)
-        {
-            // Remove items from the inventory based on the recipe
-            foreach (Item recipeItem in items)
-            {
-                RemoveItemFromInventory(recipeItem);
-            }
-        }
-
-        private void AddItemToInventory(Item item, int count)
-        {
-            // Add the crafted item to the inventory
-            for (int i = 0; i < itemSlots.Count; i++)
-            {
-                ItemSlot slot = itemSlots[i];
-                if (slot.item == null)
-                {
-                    slot.item = item;
-                    slot.Count = count;
-                    break;
-                }
-            }
-        }
-
-        private bool ContainsItem(Item item)
-        {
-            // Check if the inventory contains a specific item
-            foreach (ItemSlot slot in itemSlots)
-            {
-                if (slot.item == item && slot.Count > 0)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        private void RemoveItemFromInventory(Item item)
-        {
-            // Remove a specific item from the inventory
-            foreach (ItemSlot slot in itemSlots)
-            {
-                if (slot.item == item && slot.Count > 0)
-                {
-                    slot.Count--;
-                    break;
-                }
-            }
-        }
-    
+        return true;
     }
+
+    private void RemoveItemsFromInventory(List<Item> items)
+    {
+        // Remove items from the inventory based on the recipe
+        foreach (Item recipeItem in items)
+        {
+            RemoveItemFromInventory(recipeItem);
+        }
+    }
+
+    private void AddItemToInventory(Item item, int count)
+    {
+        // Add the crafted item to the inventory
+        for (int i = 0; i < itemSlots.Count; i++)
+        {
+            ItemSlot slot = itemSlots[i];
+            if (slot.item == null)
+            {
+                slot.item = item;
+                slot.Count = count;
+                break;
+            }
+        }
+    }
+
+    private bool ContainsItem(Item item)
+    {
+        // Check if the inventory contains a specific item
+        foreach (ItemSlot slot in itemSlots)
+        {
+            if (slot.item == item && slot.Count > 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void RemoveItemFromInventory(Item item)
+    {
+        // Remove a specific item from the inventory
+        foreach (ItemSlot slot in itemSlots)
+        {
+            if (slot.item == item && slot.Count > 0)
+            {
+                slot.Count--;
+                break;
+            }
+        }
+    }
+
 }
+
 
 
--- Assets/CraftingSystem/[Scripts]/Item.cs
+++ Assets/CraftingSystem/[Scripts]/Item.cs
@@ -2,36 +2,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //Attribute which allows right click->Create
-namespace CraftingSystem
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Items/New Item")]
+public class Item : ScriptableObject //Extending SO allows us to have an object which exists in the project, not in the scene
 {
-    [CreateAssetMenu(fileName = "New Item", menuName = "Items/New Item")]
-    public class Item : ScriptableObject //Extending SO allows us to have an object which exists in the project, not in the scene
+    public enum ItemType
     {
-        public enum ItemType
-        {
-            Consumable,
-            Weapon,
-            Tool,
-            // Add more item types based on your game's needs
-        }
-    
-        public ItemType itemType;
-    
-        public Sprite icon;
-        [TextArea]
-        public string description = "";
-        public bool isConsumable = false;
-    
-        // Add a variable to store the maximum stack size for the item
-        public int maxStackSize = 1;
+        Consumable,
+        Weapon,
+        Tool,
+        // Add more item types based on your game's needs
+    }
 
-        // Add a variable to store the crafting recipe (optional)
-        public List<Item> craftingRecipe;
+    public ItemType itemType;
+
+    public Sprite icon;
+    [TextArea]
+    public string description = "";
+    public bool isConsumable = false;
+
+    // Add a variable to store the maximum stack size for the item
+    public int maxStackSize = 1;
+
+    // Add a variable to store the crafting recipe (optional)
+    public List<Item> craftingRecipe;
 
 
-        public void Use()
-        {
-            Debug.Log("This is the Use() function of item: " + name + " - " + description);
-        }
+    public void Use()
+    {
+        Debug.Log("This is the Use() function of item: " + name + " - " + description);
     }
 }
+
